399c7e9ba4a36090dfc1e1c3bff10458478a24e6
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-07 00:24:39 +0300

    Add the hex font in memory from 0x00 to 0x50

--- src/chip8.c
+++ src/chip8.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
+#include <string.h>
 #include "chip8.h"
 
 chip8_t * chip8_new(void) {
+  int i;
   chip8_t * self = (chip8_t *) malloc(sizeof(chip8_t));
 
   // The first 512 bytes are used by the interpreter.
@@ -9,6 +11,8 @@ chip8_t * chip8_new(void) {
   self->index_register  = 0;
   self->stack_pointer   = 0;
   self->opcode          = 0;
+
+  memcpy(self->memory, chip8_hex_font, 50);
 
   return self;
 }
