9254ece6bfcf35f65959ca5f2b59d14ce53c0e7f
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-06 16:35:58 +0300

    Initial chip8 structure

---
+++ src/chip8.h
@@ -0,0 +1,15 @@
+struct chip8 {
+  unsigned short opcode;
+  unsigned char  memory[4096];
+  unsigned char  general_purpose_registers[16];
+  unsigned short index_register;
+  unsigned short program_counter;
+  unsigned char  screen[64 * 32];
+  unsigned char  delay_timer;
+  unsigned char  sound_timer;
+  unsigned short stack[16];
+  unsigned short stack_pointer;
+  unsigned char  keys[16];
+};
+
+typedef struct chip8 chip8_t;
