7acbab9891ed26c2770f8ced2369d254837e3533
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-06 23:46:18 +0300

    Screw it, I'm using C++ comments

--- src/chip8.c
+++ src/chip8.c
@@ -4,7 +4,7 @@
 chip8_t * chip8_new(void) {
   chip8_t * self = (chip8_t *) malloc(sizeof(chip8_t));
 
-  /* The first 512 bytes are used by the interpreter. */
+  // The first 512 bytes are used by the interpreter.
   self->program_counter = 0x200;
   self->index_register  = 0;
   self->stack_pointer   = 0;
