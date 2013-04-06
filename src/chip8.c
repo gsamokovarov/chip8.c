e1559a5b92d6db3880f6226e03aed4f5cc1182e0
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-06 23:30:47 +0300

    Define the functions on one line

--- src/chip8.c
+++ src/chip8.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include "chip8.h"
 
-chip8_t *
-chip8_new(void) {
+chip8_t * chip8_new(void) {
   chip8_t * self = (chip8_t *) malloc(sizeof(chip8_t));
 
   /* The first 512 bytes are used by the interpreter. */
@@ -14,7 +13,6 @@ chip8_new(void) {
   return self;
 }
 
-void
-chip8_free(chip8_t * self) {
+void chip8_free(chip8_t * self) {
   free(self);
 }
