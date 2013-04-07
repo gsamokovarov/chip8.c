d59e36a26fc3833b90f0bf38d199fc5bc9768a77
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-07 22:33:08 +0300

    Implement and test 0x8XY5 (SUBTRACTION WITH BORROW)

--- test/test_chip8.c
+++ test/test_chip8.c
@@ -270,6 +270,24 @@ void test_set_vx_to_vx_plus_vy(void) {
   chip8_free(chip8);
 }
 
+void test_set_vx_to_vx_sub_vy(void) {
+  chip8_t * chip8 = chip8_new();
+
+  chip8->memory[0x200] = 0x81;
+  chip8->memory[0x201] = 0x25;
+
+  chip8->registers[1] = 0x42;
+  chip8->registers[2] = 0xFF;
+
+  chip8_fetch_current_opcode(chip8);
+  chip8_decode_current_opcode(chip8);
+
+  assert(chip8->registers[1] == (unsigned char) (0x42 - 0xFF));
+  assert(chip8->registers[15] == 0);
+
+  chip8_free(chip8);
+}
+
 int main(int argc, char ** argv) {
   test_clear_screen();
   test_return();
@@ -285,6 +303,7 @@ int main(int argc, char ** argv) {
   test_set_vx_to_vx_and_vy();
   test_set_vx_to_vx_xor_vy();
   test_set_vx_to_vx_plus_vy();
+  test_set_vx_to_vx_sub_vy();
 
   return 0;
 }
