7bcaca009efe0b1f7149aff2b14655772fc9c37e
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-07 20:12:49 +0300

    Test 0x8XY{0,1}

--- test/test_chip8.c
+++ test/test_chip8.c
@@ -164,6 +164,40 @@ void test_set_vx_to_vx_plus_kk(void) {
   chip8_free(chip8);
 }
 
+void test_set_vx_to_vy(void) {
+  chip8_t * chip8 = chip8_new();
+
+  chip8->memory[0x200] = 0x81;
+  chip8->memory[0x201] = 0x20;
+
+  chip8->registers[1] = 0x00;
+  chip8->registers[2] = 0x42;
+
+  chip8_fetch_current_opcode(chip8);
+  chip8_decode_current_opcode(chip8);
+
+  assert(chip8->registers[1] == 0x42);
+
+  chip8_free(chip8);
+}
+
+void test_set_vx_to_vx_xor_vy(void) {
+  chip8_t * chip8 = chip8_new();
+
+  chip8->memory[0x200] = 0x81;
+  chip8->memory[0x201] = 0x21;
+
+  chip8->registers[1] = 0x00;
+  chip8->registers[2] = 0x42;
+
+  chip8_fetch_current_opcode(chip8);
+  chip8_decode_current_opcode(chip8);
+
+  assert(chip8->registers[1] == (0x00 | 0x42));
+
+  chip8_free(chip8);
+}
+
 int main(int argc, char ** argv) {
   test_clear_screen();
   test_instruction_jump();
@@ -173,6 +207,8 @@ int main(int argc, char ** argv) {
   test_skip_next_if_vx_is_vy();
   test_set_vx_to_kk();
   test_set_vx_to_vx_plus_kk();
+  test_set_vx_to_vy();
+  test_set_vx_to_vx_xor_vy();
 
   return 0;
 }
