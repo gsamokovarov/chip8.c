2bacc5444cfc0f88c980d0b9c39c63ce5cf2e4fb
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-07 23:09:06 +0300

    Make sure to get the next op for all 0x8XYN

--- src/chip8.c
+++ src/chip8.c
@@ -122,6 +122,7 @@ void chip8_decode_current_opcode(chip8_t * self) {
         self->registers[(self->opcode & 0x0F00) >> 8];
       break;
     }
+    chip8_next_opcode(self);
     break;
   case 0xA000:
     self->index_register = self->opcode & 0x0FFF;
