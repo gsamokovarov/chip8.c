07824c08b68e5b996897a477972e2b7f10ca2a62
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-16 20:03:00 +0300

    Typedef the function pointer, not the fn itself

--- src/io.h
+++ src/io.h
@@ -6,12 +6,12 @@
 typedef struct chip8 chip8_t;
 typedef struct io io_t;
 
-typedef void (io_render_t)(io_t *, chip8_t *);
-typedef void (io_listen_t)(io_t *, chip8_t *);
+typedef void (* io_render_t)(io_t *, chip8_t *);
+typedef void (* io_listen_t)(io_t *, chip8_t *);
 
 typedef struct io {
-  io_render_t * render;
-  io_listen_t * listen;
+  io_render_t render;
+  io_listen_t listen;
 } io_t;
 
 void io_free(io_t *);
