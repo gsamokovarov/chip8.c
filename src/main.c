cdac39ec978c2829fb6d964c97e5ca7f3d50def9
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-18 22:16:29 +0300

    Explicitly choose IO on main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 #include "chip8.h"
 #include "io.h"
 #include "io/terminal_io.h"
@@ -11,12 +12,20 @@ int chip8_is_running = 1;
 
 int main(int argc, char ** argv) {
   chip8_t * chip8 = chip8_new();
-  io_t * io = sdl_io_new();
+  io_t * io = 0;
 
-  if (argc == 2) {
-    if (!chip8_load_file(chip8, argv[1])) {
+  if (argc == 3) {
+    if (!chip8_load_file(chip8, argv[2])) {
       goto error;
     };
+    if (strcmp(argv[1], "sdl") == 0) {
+      io = sdl_io_new();
+    } else if (strcmp(argv[1], "terminal") == 0) {
+      io = terminal_io_new();
+    } else {
+      fprintf(stderr, "Invalid io, use sdl or terminal");
+      goto error;
+    }
     signal(SIGINT, handle_interrupt);
     io_setup(io);
     while (chip8_is_running) {
@@ -26,18 +35,24 @@ int main(int argc, char ** argv) {
       usleep(167);
     }
   } else {
-    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    fprintf(stderr, "Usage: %s <sdl|terminal> <filename>\n", argv[0]);
   }
 
-  io_teardown(io);
   chip8_free(chip8);
-  io_free(io);
+  if (io) {
+    io_teardown(io);
+    io_free(io);
+  }
+
   return 0;
 
 error:
-  io_teardown(io);
   chip8_free(chip8);
-  io_free(io);
+  if (io) {
+    io_teardown(io);
+    io_free(io);
+  }
+
   return 1;
 }
 
