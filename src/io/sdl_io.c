f73ce8d1cf22fcbf0d4e47b6af286d4121493d66
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-20 14:53:00 +0300

    Try to use double buffering

--- src/io/sdl_io.c
+++ src/io/sdl_io.c
@@ -21,7 +21,7 @@ io_t * sdl_io_new(void) {
 void sdl_io_setup(io_t * self) {
   SDL_Init(SDL_INIT_EVERYTHING);
 
-  SDL_IO_CUSTOM(self)->surface = SDL_SetVideoMode(64 * 8, 32 * 8, 32, SDL_HWSURFACE);
+  SDL_IO_CUSTOM(self)->surface = SDL_SetVideoMode(64 * 8, 32 * 8, 32, SDL_HWSURFACE|SDL_DOUBLEBUF);
 }
 
 void sdl_io_render(io_t * self, chip8_t * chip8) {
