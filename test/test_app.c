3f6405a7ebcb804e88aba4be5a68c767690c4a72
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-04-30 16:46:13 +0300

    Add a failing test for app_parse_command_line

--- test/test_app.c
+++ test/test_app.c
@@ -1,4 +1,6 @@
+#include <string.h>
 #include "test.h"
+#include "io.h"
 #include "app.h"
 
 TEST(app_new) {
@@ -23,7 +25,19 @@ TEST(current_app_set_to) {
   current_app_set_to(0);
 }
 
+TEST(app_parse_command_line) {
+  app_t * app = app_new();
+  int     argc = 3;
+  char  * argv[] = {"chip8", "--io", "terminal"};
+
+  app_parse_command_line(app, argc, argv);
+  assert(app->io && strcmp(app->io->name, "terminal") == 0);
+
+  app_free(app);
+}
+
 static test_t app_test_suite[] = {
   TEST_REF(app_new),
+  TEST_REF(app_parse_command_line),
   0
 };
