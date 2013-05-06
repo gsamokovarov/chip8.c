f0d907e3c7b4b4580824f5bb0183871aadb4f72b
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-05-06 12:25:35 +0300

    Cast the malloc of sdl_io->custom

--- src/io/sdl_io.c
+++ src/io/sdl_io.c
@@ -15,7 +15,7 @@ io_t * sdl_io_new(void) {
   self->listen   = &sdl_io_listen;
   self->delay    = &sdl_io_delay;
   self->teardown = &sdl_io_teardown;
-  self->custom   = malloc(sizeof(sdl_io_custom_t));
+  self->custom   = (sdl_io_custom_t *) malloc(sizeof(sdl_io_custom_t));
 
   return self;
 }
